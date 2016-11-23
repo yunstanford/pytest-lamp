d9cef3b1b7715eba2c1018f96ef052f5ea572f7d
Yun Xu <yunx@zillowgroup.com>
2016-11-22 16:33:12 -0800

    Initial Commit

---
+++ pytest_lamp/decorators.py
@@ -0,0 +1,23 @@
+import asyncio
+
+def asyncserver(host, port, server_handler=None):
+
+    def decorator(unittest):
+
+        async def default_handler(reader, writer):
+            data = (await reader.read())
+            writer.write(data)
+            await writer.drain()
+            writer.close()
+
+        async def inner(*args, **kwargs):
+            import asyncio
+            handler = server_handler or default_handler
+            server = await asyncio.start_server(handler, host,
+                                                port)
+            await unittest(*args, **kwargs)
+            server.close()
+
+        return inner
+
+    return decorator
